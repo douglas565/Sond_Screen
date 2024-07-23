61cd9059fc3e75183c18c16c991055c3cc61b854
Douglas Ramos <douglasramos16@outlook.com>
2024-07-23 17:42:29 -0300

    Swith

--- Sond_Screen/Program.cs
+++ Sond_Screen/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound";
 
-void ExibirMensagemDeBoasVindas()
+void ExibirLogo()
 {
     Console.WriteLine(@"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -16,6 +16,8 @@ void ExibirMensagemDeBoasVindas()
 
 void ExibirOpcoesDoMenu()
 {
+    ExibirLogo();
+
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma bandas");
@@ -29,7 +31,7 @@ void ExibirOpcoesDoMenu()
 
     switch (opcaoEscolhidaNumerica)
     {
-        case 1: Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
+        case 1: RegistrarBanda();
             break;
         case 2: Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
             break;
@@ -37,7 +39,7 @@ void ExibirOpcoesDoMenu()
             break;
         case 4: Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
             break;
-        case -1: Console.WriteLine("Você escolheu a opção: " + opcaoEscolhidaNumerica);
+        case -1: Console.WriteLine("Adeus, espero te ver outra vez...");
             break;
         default: Console.WriteLine("Opção Invalida");
             break;
@@ -45,7 +47,16 @@ void ExibirOpcoesDoMenu()
 
 }
 
+void RegistrarBanda()
+{
+    Console.Clear();
+    Console.WriteLine("Registro de Bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
 
-
-ExibirMensagemDeBoasVindas();
 ExibirOpcoesDoMenu();
