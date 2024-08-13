1160310473d316ff9aa99d193ca125595246f523
Douglas Ramos <douglasramos16@outlook.com>
2024-08-13 11:44:30 -0300

    ...

---
+++ Sond_Screen/Musica.cs
@@ -0,0 +1,27 @@
+﻿//nome
+//artista
+//duracao
+//disponivel
+
+class Musica
+{ 
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome:{nome}");
+        Console.WriteLine($"Artista:{artista}");
+        Console.WriteLine($"Duração:{duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Indisponivel no plano, adiquire o plano Plus+.");
+        }
+    }
+}
