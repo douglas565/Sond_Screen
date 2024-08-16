a38cbdd0ef6a0298275029aff747b465765535a9
Douglas Ramos <douglasramos16@outlook.com>
2024-08-16 14:31:11 -0300

    ..

---
+++ Sond_Screen/Banda.cs
@@ -0,0 +1,19 @@
+﻿class Banda 
+{
+    private List<Album> albums = new List<Album>();
+    public string Nome { get; set; }
+
+    public void AdicionarAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
+    public void ExibicaoDiscografia()
+    {
+        Console.WriteLine($"Discocrafia da banda {Nome}");
+        foreach(Album album in albums)
+        {
+            Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
